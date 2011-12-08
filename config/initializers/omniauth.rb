d17a8de9c5505bb8a101ffc23d89d6b7fa02eb50
Jesse Pollak <jpollak92@gmail.com>
2011-12-07 17:02:10 -0800

    changed facebook login from popup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '300857556614018', 'd6caee5bc42d9358507ffa597422be07', :display => "popup"
+  provider :facebook, '300857556614018', 'd6caee5bc42d9358507ffa597422be07'
 end
