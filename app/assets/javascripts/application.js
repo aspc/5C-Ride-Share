369455efef0d4cd2966f61113654decccbadbffe
Jesse Pollak <jpollak92@gmail.com>
2011-12-08 16:27:18 -0800

    fixed ga

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,27 +8,3 @@
 //= require jquery_ujs
 //= require_tree .
 
-function popupCenter(url, width, height, name) {
-  var left = (screen.width/2)-(width/2);
-  var top = (screen.height/2)-(height/2);
-  return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
-}
-
-$("a.popup").click(function(e) {
-  popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
-  e.stopPropagation(); return false;
-});
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-27594450-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
