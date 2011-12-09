a79b05a7f567d8c6eac80197f6655e9d94e8bb27
Jesse Pollak <jpollak92@gmail.com>
2011-12-08 19:16:22 -0800

    change fb_id to string

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '300857556614018', 'd6caee5bc42d9358507ffa597422be07', :scope => 'email'
+  provider :facebook, '195519233854114', 'dbe88f55b3ea0616733205d457f1e723', :scope => 'email'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111207231221) do
+ActiveRecord::Schema.define(:version => 20111209031021) do
 
   create_table "rides", :force => true do |t|
     t.string   "airport"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(:version => 20111207231221) do
   end
 
   create_table "users", :force => true do |t|
-    t.integer  "fb_id"
+    t.string   "fb_id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
