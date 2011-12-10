2b46cda2c23f04cffb47299cb4009d240bb9ba79
Jesse Pollak <jpollak92@gmail.com>
2011-12-10 12:08:05 -0800

    invalidated cookies

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Shuttleshare::Application.config.secret_token = '8e85189dc3dcf50a77cb6cd123371eb76419a3d430ae544fdd047543cbe7fbe877119968f089ce73a44e6c735cd2ed807f43743faa8188510653bd623a0780f0'
+Shuttleshare::Application.config.secret_token = '1e85189dc3dcf50a77cb6cd123371eb76419a3d430ae544fdd047543cbe7fbe877119968f089ce73a44e6c735cd2ed807f43743faa8188510653bd623a0780f0'
