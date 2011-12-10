0aaa0cca5c8169a75180660a218bf8f9c7f091e3
Jesse Pollak <jpollak92@gmail.com>
2011-12-10 12:40:36 -0800

    hid gmail password

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ Shuttleshare::Application.initialize!
 
 
 ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
+  :address => 'smtp.gmail.com',
   :port => 587,
   :domain => "gmail.com",
   :user_name => "5crideshare@gmail.com",
-  :password => "olney03pumas",
-  :authentication => "plain",
+  :password => YAML.load_file("#{Rails.root}/config/email.credentials.yml")['password'],
+  :authentication => 'plain',
   :enable_starttls_auto => true
 }
