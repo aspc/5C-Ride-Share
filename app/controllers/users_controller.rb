92c3673b61e4f1a51e6ee6850df9e1445ca234e4
Jesse Pollak <jpollak92@gmail.com>
2011-12-19 16:30:03 -0500

    logged out styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  before_filter :correct_user, :only => [:edit, :show]
+  
   def edit
     @title = "5C ride share :: settings"
   end
+  
   def update
     if @current_user.update_attributes(params[:user])
       redirect_to edit_user_path(@current_user), :notice => "Your user information was successfully updated."
@@ -9,10 +12,19 @@ class UsersController < ApplicationController
       redirect_to :action => 'edit'
     end
   end
+  
   def show
     @title = "5C ride share :: your rides"
     @rides = @current_user.rides
   end
   def rides
   end
+  
+  protected
+  
+    helper_method :correct_user
+    
+    def correct_user
+      redirect_to root_url unless @current_user && params[:id] == @current_user.id.to_s
+    end
 end
