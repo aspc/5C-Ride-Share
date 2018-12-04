3a26a1d85d4f71694bb5ada68e264b6e9e9ce19f
Adam <agsmguo@gmail.com>
2018-12-04 09:08:34 -0800

    Add login form

--- db/schema.rb
+++ db/schema.rb
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 20181203225544) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "rides", force: :cascade do |t|
-    t.string   "airport",    limit: 255
+    t.string   "airport"
     t.datetime "flighttime"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "owner_id"
     t.time     "ridetime"
   end
@@ -43,15 +43,15 @@ ActiveRecord::Schema.define(version: 20181203225544) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                 limit: 255
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
-    t.string   "email",                limit: 255
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email"
     t.boolean  "email_pref"
     t.datetime "logged_out_at"
-    t.boolean  "is_cas_authenticated",             default: false, null: false
-    t.boolean  "is_admin",                         default: false, null: false
-    t.integer  "school",                           default: 0,     null: false
+    t.boolean  "is_cas_authenticated", default: false, null: false
+    t.boolean  "is_admin",             default: false, null: false
+    t.integer  "school",               default: 0,     null: false
     t.string   "password"
   end
 
