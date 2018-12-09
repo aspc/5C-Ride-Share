0da454e82c7b2c7a64823f6a01638dd09b01c2cf
Bradley Bain <brad_bain@live.com>
2018-12-09 15:30:47 -0800

    Create basic active admin custom page scaffold

---
+++ app/admin/aspc_free_ride_program.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register_page "ASPC Free Ride Program" do
+
+  menu priority: 2
+
+  content do
+    columns do
+      column do
+        panel "Program Timeframe" do
+
+        end
+      end
+    end
+  end
+end
