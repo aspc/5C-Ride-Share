254528590ebcb8cabbc4e809bdc4aca1c7e7ef92
Bradley Bain <brad_bain@live.com>
2019-12-08 18:50:19 -0800

    Export riders in csv

--- app/admin/rides.rb
+++ app/admin/rides.rb
@@ -4,6 +4,44 @@ ActiveAdmin.register Ride do
   permit_params :airport, :flighttime, :owner_id, :ridetime, :is_aspc, :terminal,
                 user_ids: []
 
+
+  index do
+    selectable_column
+    column :id
+    column :airport
+    column :flighttime
+    column "Owner" do |ride|
+      link_to "#{ride.owner.name} (#{ride.owner.email})", admin_user_path(ride.owner_id)
+    end
+    column "Riders" do |ride|
+      ride.users.each do |user|
+        a user.name, href: admin_user_path(user)
+        text_node "&nbsp;".html_safe
+      end
+    end
+    column :ridetime
+    column :is_aspc
+    column :terminal
+  end
+
+  csv do
+    column :id
+    column :airport
+    column :terminal
+    column :flighttime
+    column "preferred ride time" do |ride|
+      ride.ridetime
+    end
+    column :owner  do |ride|
+      "#{ride.owner.name} #{ride.owner.email}"
+    end
+    column "riders" do |ride|
+      ride.users.map { |user| "#{user.name} (#{user.email})" }.join(",")
+    end
+    column :is_aspc
+  end
+
+
   sidebar "Nested Resources", only: [:show, :edit] do
     ul do
       li link_to "Riders",    admin_ride_users_path(resource)
