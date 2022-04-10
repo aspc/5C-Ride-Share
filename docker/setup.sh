7916093d4942a1da943b255133e5b3bf33abc68b
Adam Guo <agsmguo@gmail.com>
2022-04-09 18:14:01 -0700

    Remove setup_completion_marker and run rake db:create/migrate always

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-FILE=/claremontrideshare/docker/setup_completion_marker
-
-setupDB() {
-    echo "Setting up DB"
-    bundle exec rake db:create 
-    bundle exec rake db:migrate
-    touch $FILE 
-}
-
-dbIsSetup() {
-    if test -f $FILE; then 
-    return 0
-    else return 1
-    fi
-}
-
-dbIsSetup && echo "Database is Already Setup!" || setupDB
+echo "Setting up DB"
+bundle exec rake db:create 
+bundle exec rake db:migrate
 echo "Setup Complete"
-
-
-
-
-
